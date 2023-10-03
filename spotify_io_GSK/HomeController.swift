2aaac0d053ec1f3ff23805fcecc4cdc6666c054f
wensaqt <captainrandomshit@gmail.com>
2023-10-03 16:57:28 +0200

    created home page collection and music cells, interfaces and extensions too

--- spotify_io_GSK/HomeController.swift
+++ spotify_io_GSK/HomeController.swift
@@ -7,13 +7,56 @@
 
 import UIKit
 
-class HomeController: UIViewController {
+class HomeController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    struct Music {
+        var title: String
+        var imagePlaceholder: UIColor
+    }
+    
+    var collectionView: UICollectionView!
+    
+    var songs: [Music] = [
+        Music(title: "Chanson 1", imagePlaceholder: .green),
+        Music(title: "Chanson 2", imagePlaceholder: .green),
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(named: "#191414")
+        
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: (view.frame.size.width/2) - 15, height: 200) // Ajustez la hauteur selon vos besoins
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(MusicCellController.self, forCellWithReuseIdentifier: "MusicCell")
+        collectionView.backgroundColor = UIColor(named: "#191414")
+
+        view.addSubview(collectionView)
+
+        // Configuration des contraintes
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10)
+        ])
+    }
+}
+
+extension HomeController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return songs.count
     }
 
-
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MusicCell", for: indexPath) as! MusicCellController
+        cell.titleLabel.text = songs[indexPath.row].title
+        cell.imageView.backgroundColor = songs[indexPath.row].imagePlaceholder
+        return cell
+    }
 }
+
 
---
+++ spotify_io_GSK/MusicCellController.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+class MusicCellController: UICollectionViewCell {
+    
+    var imageView: UIView!  // Placeholder pour l'image
+    var titleLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupImageView()
+        setupTitleLabel()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupImageView() {
+        imageView = UIView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = .green  // La couleur placeholder
+        contentView.addSubview(imageView)
+        
+        NSLayoutConstraint.activate([
+            imageView.widthAnchor.constraint(equalToConstant: 100),
+            imageView.heightAnchor.constraint(equalToConstant: 100),
+            imageView.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor),
+        ])
+    }
+    
+    private func setupTitleLabel() {
+        titleLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textColor = .black
+        titleLabel.textAlignment = .center
+        contentView.addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8),
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            titleLabel.bottomAnchor.constraint(lessThanOrEqualTo: contentView.bottomAnchor)
+        ])
+    }
+}
+
+
