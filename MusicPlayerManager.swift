50c768cd67ea02ae259c69dc89ea478d71257d9a
Kevw94 <pinerokevin@icloud.com>
2023-10-05 10:18:47 +0200

    Implement manager to listen one by one music

---
+++ MusicPlayerManager.swift
@@ -0,0 +1,44 @@
+//
+//  MusicPlayerManager.swift
+//  spotify_io_GSK
+//
+//  Created by kevin on 04/10/2023.
+//
+
+import Foundation
+import AVKit
+
+class MusicPlayerManager {
+    static let shared = MusicPlayerManager()
+    
+    public var audioPlayer: AVPlayer?
+    
+    var isPlaying: Bool {
+        return audioPlayer?.rate != 0 && audioPlayer?.error == nil
+    }
+    
+    func playMusic(with music: Music?) {
+        if let musicURL = URL(string: music?.preview ?? "") {
+            let playerItem = AVPlayerItem(url: musicURL)
+            audioPlayer = AVPlayer(playerItem: playerItem)
+            
+            audioPlayer?.play()
+        }
+    }
+    
+    func play() {
+        audioPlayer?.play()
+    }
+    
+    func pause() {
+        audioPlayer?.pause()
+    }
+    
+    func seek(to time: Double) {
+        let cmTime = CMTime(seconds: time, preferredTimescale: 1)
+        audioPlayer?.seek(to: cmTime)
+    }
+
+    
+    
+}
--- spotify_io_GSK/MusicPlayerController.swift
+++ spotify_io_GSK/MusicPlayerController.swift
@@ -8,31 +8,28 @@ class MusicPlayerController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var artistLabel: UILabel!
     @IBOutlet weak var slider: UISlider!
-
+    
     @IBOutlet weak var controlButton: UIButton!
     
     var musics: [Music]?
     var indexSound: Int?
     let playImage = UIImage(systemName: "play")
     let pauseImage = UIImage(systemName: "pause")
-
+    
+    var musicPlayerManager = MusicPlayerManager.shared
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .white
-        if let musicURL = URL(string: self.musics?[self.indexSound!].preview ?? "") {
-            let playerItem = AVPlayerItem(url: musicURL)
-            self.audioPlayer = AVPlayer(playerItem: playerItem)
-            
-        }
+        musicPlayerManager.playMusic(with: self.musics?[self.indexSound!])
+        
         self.slider!.minimumValue = 0
         
         
         // SET slider to 0 initial value
         self.slider.value = 0
-        if let duration = self.audioPlayer?.currentItem?.asset.duration {
+        if let duration = musicPlayerManager.audioPlayer?.currentItem?.asset.duration {
             let durationInSeconds = CMTimeGetSeconds(duration)
             self.slider.maximumValue = Float(durationInSeconds)
         }
@@ -40,23 +37,23 @@ class MusicPlayerController: UIViewController {
         self.slider!.tintColor = UIColor.green
         
         
-        NotificationCenter.default.addObserver(self, selector: #selector(playerDidFinishPlaying(_:)), name: .AVPlayerItemDidPlayToEndTime, object: audioPlayer?.currentItem)
-        self.audioPlayer!.addPeriodicTimeObserver(forInterval: CMTimeMakeWithSeconds(1, preferredTimescale: 1), queue: DispatchQueue.main) { (CMTime) -> Void in
-            if self.audioPlayer!.currentItem?.status == .readyToPlay {
-                let time : Float64 = CMTimeGetSeconds(self.audioPlayer!.currentTime());
+        NotificationCenter.default.addObserver(self, selector: #selector(playerDidFinishPlaying(_:)), name: .AVPlayerItemDidPlayToEndTime, object: musicPlayerManager.audioPlayer?.currentItem)
+        musicPlayerManager.audioPlayer!.addPeriodicTimeObserver(forInterval: CMTimeMakeWithSeconds(1, preferredTimescale: 1), queue: DispatchQueue.main) { (CMTime) -> Void in
+            if self.musicPlayerManager.audioPlayer!.currentItem?.status == .readyToPlay {
+                let time : Float64 = CMTimeGetSeconds(self.musicPlayerManager.audioPlayer!.currentTime());
                 self.slider!.value = Float ( time );
             }
+            
         }
-        self.audioPlayer?.play()
         self.controlButton.setImage(self.pauseImage, for: .normal)
         self.controlButton.isSelected = true
-
+        
     }
-
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         setupUI()
-
+        
     }
     
     
@@ -81,34 +78,30 @@ class MusicPlayerController: UIViewController {
     }
     
     @IBAction func sliderChange(_ sender: UISlider) {
-        let seconds : Int64 = Int64(slider.value)
-        let targetTime:CMTime = CMTimeMake(value: seconds, timescale: 1)
-                
-        self.audioPlayer!.seek(to: targetTime)
+        let seconds : Double = Double(slider.value)
+        musicPlayerManager.seek(to: seconds)
     }
     @IBAction func playButton(_ sender: UIButton) {
-            if controlButton.isSelected {
-                controlButton.isSelected = false
-                print(sender.isSelected)
-                controlButton.setImage(self.playImage, for: .normal)
-                self.audioPlayer?.pause()
-            }
-            else {
-                controlButton.isSelected = true
-                print(sender.isSelected)
-                controlButton.setImage(self.pauseImage, for: .normal)
-                self.audioPlayer?.play()
-            }
+        if controlButton.isSelected {
+            controlButton.isSelected = false
+            controlButton.setImage(self.playImage, for: .normal)
+            musicPlayerManager.pause()
         }
+        else {
+            controlButton.isSelected = true
+            controlButton.setImage(self.pauseImage, for: .normal)
+            musicPlayerManager.play()
+        }
+    }
     
-
+    
     
     func setupUI() {
         self.titleLabel.text = musics?[indexSound!].title
         self.artistLabel.text = musics?[indexSound!].artist.name
-
+        
         if let imageUrl = musics?[indexSound!].image {
-          imgAlbumCover.downloaded(from: imageUrl)
+            imgAlbumCover.downloaded(from: imageUrl)
         }
     }
     
@@ -124,32 +117,22 @@ class MusicPlayerController: UIViewController {
     }
     
     func playNextTrack() {
-           if self.indexSound! < self.musics?.count ?? 0 {
-               if let musicURL = URL(string: self.musics?[self.indexSound!].preview ?? "") {
-                   let playerItem = AVPlayerItem(url: musicURL)
-                   
-                   
-                   
-                   self.audioPlayer?.pause()
-                   self.audioPlayer = nil
-                   
-                   self.audioPlayer = AVPlayer(playerItem: playerItem)
-               }
-
-               
-               NotificationCenter.default.addObserver(self, selector: #selector(playerDidFinishPlaying), name: .AVPlayerItemDidPlayToEndTime, object: audioPlayer?.currentItem)
-               
-               self.audioPlayer!.addPeriodicTimeObserver(forInterval: CMTimeMakeWithSeconds(1, preferredTimescale: 1), queue: DispatchQueue.main) { (CMTime) -> Void in
-                   if self.audioPlayer!.currentItem?.status == .readyToPlay {
-                       let time : Float64 = CMTimeGetSeconds(self.audioPlayer!.currentTime());
-                       self.slider!.value = Float ( time );
-                   }
-               }
-
-               audioPlayer?.play()
-               self.controlButton.isSelected = true
-               self.controlButton.setImage(self.pauseImage, for: .normal)
-               self.slider.value = 0
-           }
-       }
+        if self.indexSound! < self.musics?.count ?? 0 {
+            musicPlayerManager.playMusic(with: self.musics?[self.indexSound!])
+            
+            NotificationCenter.default.addObserver(self, selector: #selector(playerDidFinishPlaying), name: .AVPlayerItemDidPlayToEndTime, object: musicPlayerManager.audioPlayer?.currentItem)
+            
+            musicPlayerManager.audioPlayer!.addPeriodicTimeObserver(forInterval: CMTimeMakeWithSeconds(1, preferredTimescale: 1), queue: DispatchQueue.main) { (CMTime) -> Void in
+                if self.musicPlayerManager.audioPlayer!.currentItem?.status == .readyToPlay {
+                    let time : Float64 = CMTimeGetSeconds(self.musicPlayerManager.audioPlayer!.currentTime());
+                    self.slider!.value = Float ( time );
+                }
+            }
+            
+        
+            self.controlButton.isSelected = true
+            self.controlButton.setImage(self.pauseImage, for: .normal)
+            self.slider.value = 0
+        }
+    }
 }
